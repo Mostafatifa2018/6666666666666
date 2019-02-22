b2ebd160ab9ee5526b126c62ebdc34dd3f6cda7c
Mostafatifa2018 <47144958+Mostafatifa2018@users.noreply.github.com>
2019-02-22 10:56:38 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("حط الاي دي حق الروم")
+let channel =     client.channels.get("548076813075742720")
 setInterval(function() {
-channel.send(`حط الكلام اللي تبغاه`);
+channel.send(`احا الشبشب ضاع فء`);
 }, 30)
 })
 
@@ -15,7 +15,7 @@ client.on("message", function(message) {
     var args = message.content.split(/ +/g);
     var command = args.shift()
     
-    if(command == "حط الامر لسحب الكريديت هنا") {
+    if(command == "-تحدث") {
         message.channel.send(args.slice(1, args.length).join(" "))    
     }
 });
